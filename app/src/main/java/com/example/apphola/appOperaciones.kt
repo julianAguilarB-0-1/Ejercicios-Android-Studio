b8a2d22bdc964548c48e437f55cf33b90a302cd8
Julián Aguilar <julian.aguilar0127@gmail.com>
2026-05-29 13:58:48 -0700

    Se declaró los objetos en la clase appOperaciones

--- app/src/main/java/com/example/apphola/appOperaciones.kt
+++ app/src/main/java/com/example/apphola/appOperaciones.kt
@@ -1,12 +1,27 @@
 package com.example.apphola
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class appOperaciones : AppCompatActivity() {
+
+    private lateinit var txtNum1 : EditText
+    private lateinit var txtNum2: EditText
+    private lateinit var spnOperaciones: Spinner
+    private lateinit var img : ImageView
+    private lateinit var txtResultado : TextView
+    private lateinit var btnCalcular: Button
+    private lateinit var btnLimpiar: Button
+    private lateinit var btnCerrar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
