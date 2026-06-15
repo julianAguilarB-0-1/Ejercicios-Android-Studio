28c7f058f4a746847b77c439e3de81970319c250
Julián Aguilar <julian.aguilar0127@gmail.com>
2026-06-15 13:59:18 -0700

    Se realizo la codificación del CuentaBanco Activity

--- app/src/main/java/com/example/apphola/CuentabancoActivity.kt
+++ app/src/main/java/com/example/apphola/CuentabancoActivity.kt
@@ -1,20 +1,144 @@
 package com.example.apphola
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.appholamundo.CuentaBanco
 
 class CuentabancoActivity : AppCompatActivity() {
+
+    private lateinit var txtUsuario: TextView
+
+    private lateinit var txtNumCuenta: EditText
+    private lateinit var txtNombreCliente: EditText
+    private lateinit var txtBanco: EditText
+    private lateinit var txtSaldo: EditText
+
+    private lateinit var rdbDeposito: RadioButton
+    private lateinit var rdbRetiro: RadioButton
+    private lateinit var rdbConsulta: RadioButton
+
+    private lateinit var txtNSaldo: TextView
+    private lateinit var txtCBCantidad: EditText
+
+    private lateinit var btnRegistrarCuenta: Button
+    private lateinit var btnMovimiento: Button
+
+    private var cuenta = CuentaBanco()
+    private var cuentaRegistrada = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_cuentabanco)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        iniciarComponentes()
+        eventosClick()
+    }
+
+    fun iniciarComponentes() {
+
+        txtUsuario = findViewById(R.id.txtUsuario)
+
+        txtNumCuenta = findViewById(R.id.txtNumCuenta)
+        txtNombreCliente = findViewById(R.id.txtNombreCliente)
+        txtBanco = findViewById(R.id.txtBanco)
+        txtSaldo = findViewById(R.id.txtSaldo)
+
+        rdbDeposito = findViewById(R.id.rdbDeposito)
+        rdbRetiro = findViewById(R.id.rdbRetiro)
+        rdbConsulta = findViewById(R.id.rdbConsulta)
+
+        txtNSaldo = findViewById(R.id.txtNSaldo)
+        txtCBCantidad = findViewById(R.id.txtCBCantidad)
+
+        btnRegistrarCuenta = findViewById(R.id.btnRegistrarCuenta)
+        btnMovimiento = findViewById(R.id.btnMovimiento)
+
+        txtUsuario.text =
+            "Usuario: " + intent.getStringExtra("usuario")
+    }
+
+    fun eventosClick() {
+
+        btnRegistrarCuenta.setOnClickListener {
+
+            if (txtNumCuenta.text.toString().isEmpty() ||
+                txtNombreCliente.text.toString().isEmpty() ||
+                txtBanco.text.toString().isEmpty() ||
+                txtSaldo.text.toString().isEmpty()
+            ) {
+
+                Toast.makeText(
+                    this,
+                    "Faltó capturar algún dato",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            } else {
+
+                cuenta.numCuenta =
+                    txtNumCuenta.text.toString().toInt()
+
+                cuenta.nombreCliente =
+                    txtNombreCliente.text.toString()
+
+                cuenta.banco =
+                    txtBanco.text.toString()
+
+                cuenta.saldo =
+                    txtSaldo.text.toString().toFloat()
+
+                cuentaRegistrada = true
+
+                txtNSaldo.text =
+                    "Nuevo Saldo: $" + cuenta.obtenerSaldo()
+                Toast.makeText(
+                    this, "Cuenta registrada correctamente",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnMovimiento.setOnClickListener {
+            if (!cuentaRegistrada) {
+                Toast.makeText(this, "Primero registre una cuenta",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                if (rdbConsulta.isChecked) {
+                    txtNSaldo.text = "Nuevo Saldo: $" + cuenta.obtenerSaldo()
+                } else {
+                    if (txtCBCantidad.text.toString().isEmpty()) {
+                        Toast.makeText(
+                            this, "Capture una cantidad",
+                            Toast.LENGTH_SHORT).show()
+
+                    } else {
+                        val cantidad = txtCBCantidad.text.toString().toFloat()
+                        if (rdbDeposito.isChecked) {
+                            cuenta.hacerDeposito(cantidad)
+                            txtNSaldo.text =
+                                "Nuevo Saldo: $" + cuenta.obtenerSaldo()
+                            Toast.makeText(
+                                this, "Depósito realizado",
+                                Toast.LENGTH_SHORT).show()
+                        }
+
+                        if (rdbRetiro.isChecked) {
+                            if (cuenta.retirarDinero(cantidad)) {
+                                txtNSaldo.text =
+                                    "Nuevo Saldo: $" + cuenta.obtenerSaldo()
+                                Toast.makeText(
+                                    this, "Retiro realizado",
+                                    Toast.LENGTH_SHORT).show()
+
+                            } else {
+                                Toast.makeText(this, "Saldo insuficiente",
+                                    Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
