e0286a7ee5fa4683ea6bd96ddb4571f1c223c99b
Julián Aguilar <julian.aguilar0127@gmail.com>
2026-06-15 13:03:38 -0700

    Se realizo la clase Cuenta Banco

--- app/src/main/java/com/example/apphola/CuentaBanco.kt
+++ app/src/main/java/com/example/apphola/CuentaBanco.kt
@@ -1,4 +1,32 @@
-package com.example.apphola
+package com.example.appholamundo
 
-class CuentaBanco {
+import java.io.Serializable
+
+class CuentaBanco : Serializable {
+    var numCuenta: Int = 0
+    var nombreCliente: String = ""
+    var banco: String = ""
+    var saldo: Float = 0.0f
+
+    constructor() {
+        this.numCuenta = 0
+        this.nombreCliente = ""
+        this.banco = ""
+        this.saldo = 0.0f
+    }
+
+    fun obtenerSaldo(): Float {
+        return saldo
+    }
+    fun hacerDeposito(cantidad: Float): Float {
+        saldo += cantidad
+        return saldo
+    }
+    fun retirarDinero(cantidad: Float): Boolean {
+        if (cantidad <= saldo) {
+            saldo -= cantidad
+            return true
+        }
+        return false
+    }
 }
